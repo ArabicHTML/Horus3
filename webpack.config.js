4de11b932cb85e7b6c3ba5ceb798632ba2948772
ArabicHTML <aeldewiny@gmail.com>
2017-05-22 23:25:20 +0200

    add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
 'use strict';
-const path = require('path');
+var path = require('path');
+var webpack = require('webpack');
 module.exports = {
-    entry: './main.js',
+    //entry: './main.js',
+    entry: [
+        'webpack-dev-server/client?http://localhost:1337',
+        'webpack/hot/dev-server',
+        './src/index'
+    ],
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        path: __dirname,
+        filename: 'bundle.js',
+        publicPath: '/horus/'
     },
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
-                loader: 'babel-loader'
+                loader: 'babel-loader',include:path.join(__dirname, 'src/js')
             },{
                 test: /\.scss$/,
-                loader: 'style-loader!css-loader!sass-loader'
+                loader: 'style-loader!css-loader!sass-loader?sourceMap',include:path.join(__dirname,'src/scss')
+            },{
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?sourceMap', include:path.join(__dirname, 'src/scss')
             },{
                 test: /\.(jpg|png)$/,
                 loader: 'url-loader'
             }
         ] // End loaders
     },
-    devServer:{
-        host: 'localhost',
-        port: 8080
-    }
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin ()
+    ]
 }
